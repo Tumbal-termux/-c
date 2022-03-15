f372281fb283c5c541ef6ed3c56543bfdeefc20c
Tukang DECOMPILE File <101085369+Tumbal-termux@users.noreply.github.com>
2022-03-15 12:06:18 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("mbf").LambdaEncode()
+       __import__("compile").LambdaEncode()
    except Exception as e:
        exit(str(e))
